92d36e49c48194d6db9110f494ef4cdc02376044
zengjianping <happysjfzjp@126.com>
2024-03-09 14:00:32 +0800

    dev

--- scripts/human_pose_show.sh
+++ scripts/human_pose_show.sh
@@ -4,7 +4,7 @@ ROOT_PATH=$(cd "$(dirname $(dirname "$0"))"; pwd)
 cd $ROOT_PATH
 echo "Work directory: $(pwd)"
 
-export LD_LIBRARY_PATH=/opt/MVS/lib/64:./lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
 
 #./build/examples/HumanPoseShow/HumanPoseShow.bin
 ./bin/HumanPoseShow.bin
