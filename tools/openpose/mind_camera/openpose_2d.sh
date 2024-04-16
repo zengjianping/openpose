ea08b3d9df153e3509b60b9f8c2169d653381e75
zengjianping <happysjfzjp@126.com>
2024-04-16 17:48:26 +0800

    dev

--- tools/openpose/mind_camera/openpose_2d.sh
+++ tools/openpose/mind_camera/openpose_2d.sh
@@ -5,6 +5,6 @@
     --capture_fps 60 --net_resolution "-1x256" \
     --camera_resolution "1224x1024" --crop_image \
     --trigger_save_video --image_cache_time 2.0 --trigger_save_time 0.5 \
-    --write_video ../GolfDataExchange/golf_test.mp4 \
+    --write_video /home/ezgolf/Videos/GolfDataExchange/golf_videos \
     --body 1 --num_gpu 0 --batch_process --profile_speed 300
 
